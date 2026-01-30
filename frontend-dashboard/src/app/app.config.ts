08577d00179a4a6a6fc203a24e9671950e592a98
Dagus21 <davidgustavomedinaardila@gmail.com>
2026-01-30 09:06:32 -0500

    Actualizacion app.ts y app.config.ts
    Se eliminan las caracteristicas offline , para mantener la sincronizacion de los datos

--- frontend-dashboard/src/app/app.config.ts
+++ frontend-dashboard/src/app/app.config.ts
@@ -32,10 +32,10 @@ export const appConfig: ApplicationConfig = {
         ripple: true
     }),
 
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }),
+    // provideServiceWorker('ngsw-worker.js', {
+    //     enabled: !isDevMode(),
+    //     registrationStrategy: 'registerWhenStable:30000'
+    // }),
 
     provideHttpClient(withInterceptors([
         authInterceptor, 
